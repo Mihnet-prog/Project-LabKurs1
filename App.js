5a99d9bd6bbd8ac496cc50d410080e61c89ad9fc
Mihnet <59515657+Mihnet-prog@users.noreply.github.com>
2022-04-15 07:26:00 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      **Ketu do te shkruhet kodi front edhe javascript
+    </div>
+  );
+}
+
+export default App;
